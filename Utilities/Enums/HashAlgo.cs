e905056358fbff634195dff1d3c2ad99ab57f91d
Brenden Perry <brendenperry5@gmail.com>
2024-12-15 16:45:11 -0800

    Update HashAlgo.cs

--- Utilities/Enums/HashAlgo.cs
+++ Utilities/Enums/HashAlgo.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.Foundation.Metadata;
 using System.Security.Cryptography;
 using System.ComponentModel;
 
